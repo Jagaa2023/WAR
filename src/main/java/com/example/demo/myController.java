9076c5f82243c9718f2c4e63801c654e6093eacb
Jagaa2023 <jgmobile2017@gmail.com>
2023-08-02 10:42:48 +0800

    demo2 training code

--- src/main/java/com/example/demo/myController.java
+++ src/main/java/com/example/demo/myController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = { "http://localhost:65089/"  })
+@CrossOrigin(origins = { "*"  })
 public class myController {
 	
 	@Autowired
